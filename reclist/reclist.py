8f2dc701a25922e0b6ea18045c6e039fabbe5a45
Chloe He <chloehe@stanford.edu>
2021-12-02 15:06:02 -0800

    adjust bin size; refactor

--- reclist/reclist.py
+++ reclist/reclist.py
@@ -71,80 +71,82 @@ class CoveoCartRecList(RecList):
 class SpotifySessionRecList(RecList):
 
     ########### NEXT EVENT PREDICTION #########
-    @rec_test(test_type='basic_stats')
-    def nep_stats(self):
-        """
-        Basic statistics on training, test and prediction data for Next Event Prediction
-        """
-        from reclist.metrics.standard_metrics import statistics
-        return statistics(self._x_train,
-                          self._y_train,
-                          self._x_test,
-                          self._y_test,
-                          self._y_preds)
+    # @rec_test(test_type='basic_stats')
+    # def nep_stats(self):
+    #     """
+    #     Basic statistics on training, test and prediction data for Next Event Prediction
+    #     """
+    #     from reclist.metrics.standard_metrics import statistics
+    #     return statistics(self._x_train,
+    #                       self._y_train,
+    #                       self._x_test,
+    #                       self._y_test,
+    #                       self._y_preds)
 
-    @rec_test(test_type='HR@10')
-    def hit_rate_at_k(self):
-        """
-        Compute the rate at which the top-k predictions contain the item to be predicted
-        """
-        from reclist.metrics.standard_metrics import hit_rate_at_k
-        return hit_rate_at_k(self.uri_only(self._y_preds),
-                             self.uri_only(self._y_test),
-                             k=10)
+    # @rec_test(test_type='HR@10')
+    # def hit_rate_at_k(self):
+    #     """
+    #     Compute the rate at which the top-k predictions contain the item to be predicted
+    #     """
+    #     from reclist.metrics.standard_metrics import hit_rate_at_k
+    #     return hit_rate_at_k(self.uri_only(self._y_preds),
+    #                          self.uri_only(self._y_test),
+    #                          k=10)
 
-    @rec_test(test_type='perturbation_test')
-    def perturbation_at_k(self):
-        """
-        Compute average consistency in model predictions when inputs are perturbed
-        """
-        from reclist.metrics.perturbation import session_perturbation_test
-        from collections import defaultdict
-        from functools import partial
+    # @rec_test(test_type='perturbation_test')
+    # def perturbation_at_k(self):
+    #     """
+    #     Compute average consistency in model predictions when inputs are perturbed
+    #     """
+    #     from reclist.metrics.perturbation import session_perturbation_test
+    #     from collections import defaultdict
+    #     from functools import partial
 
-        # Step 1: Generate a map from artist uri to track uri
-        substitute_mapping = defaultdict(list)
-        for track_uri, row in self.product_data.items():
-            substitute_mapping[row['artist_uri']].append(track_uri)
+    #     # Step 1: Generate a map from artist uri to track uri
+    #     substitute_mapping = defaultdict(list)
+    #     for track_uri, row in self.product_data.items():
+    #         substitute_mapping[row['artist_uri']].append(track_uri)
 
-        # Step 2: define a custom perturbation function
-        def perturb(session, sub_map):
-            last_item = session[-1]
-            last_item_artist = self.product_data[last_item['track_uri']]['artist_uri']
-            substitutes = set(sub_map.get(last_item_artist,[])) - {last_item['track_uri']}
-            if substitutes:
-                similar_item = random.sample(substitutes, k=1)
-                new_session = session[:-1] + [{"track_uri": similar_item[0]}]
-                return new_session
-            return []
+    #     # Step 2: define a custom perturbation function
+    #     def perturb(session, sub_map):
+    #         last_item = session[-1]
+    #         last_item_artist = self.product_data[last_item['track_uri']]['artist_uri']
+    #         substitutes = set(sub_map.get(last_item_artist,[])) - {last_item['track_uri']}
+    #         if substitutes:
+    #             similar_item = random.sample(substitutes, k=1)
+    #             new_session = session[:-1] + [{"track_uri": similar_item[0]}]
+    #             return new_session
+    #         return []
 
-        # Step 3: call test
-        return session_perturbation_test(self.rec_model,
-                                         self._x_test,
-                                         self._y_preds,
-                                         partial(perturb, sub_map=substitute_mapping),
-                                         self.uri_only,
-                                         k=10)
+    #     # Step 3: call test
+    #     return session_perturbation_test(self.rec_model,
+    #                                      self._x_test,
+    #                                      self._y_preds,
+    #                                      partial(perturb, sub_map=substitute_mapping),
+    #                                      self.uri_only,
+    #                                      k=10)
 
-    @rec_test(test_type='hits_distribution_by_genre')
+    @rec_test(test_type='hits_distribution_by_slice')
     def hits_distribution_by_slice(self):
         """
         Compute the distribution of hit-rate across various slices of data
         """
         from reclist.metrics.hits_slice import hits_distribution_by_slice
         
-        def generate_slices(x_test, **kwargs):
-            slices = collections.defaultdict(list)
-            for idx, playlist in enumerate(x_test):
-                slices[len(playlist)].append(idx)
-            slices = collections.OrderedDict(sorted(slices.items()))
-            return slices
+        len_map = collections.defaultdict(list)
+        for idx, playlist in enumerate(self._x_test):
+            len_map[len(playlist)].append(idx)
+        slices = collections.defaultdict(list)
+        bins = [(x * 5, (x + 1) * 5) for x in range(max(len_map) // 5 + 1)]
+        for bin_min, bin_max in bins:
+            for i in range(bin_min + 1, bin_max + 1, 1):
+                slices[f'({bin_min}, {bin_max}]'].extend(len_map[i])
+                del len_map[i]
+        assert len(len_map) == 0
 
-        return hits_distribution_by_slice(generate_slices,
-                                          self.uri_only(self._x_test),
+        return hits_distribution_by_slice(slices,
                                           self.uri_only(self._y_test),
                                           self.uri_only(self._y_preds),
-                                          self.product_data,
                                           debug=True)
 
     @rec_test(test_type='NEP_Coverage@10')
