eb5194f1ed472f015a7e3624fb48a9c2ae0b8620
vinid <chiccobia@gmail.com>
2021-12-05 16:42:11 +0100

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jacopotagliabue/reclist',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
 )
