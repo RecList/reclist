9b3fc433328133d3d70ca5efdfdfc81ad7d1ddfa
vinid <chiccobia@gmail.com>
2022-11-03 09:10:13 -0700

    Bump version: 0.2.4 → 0.2.5

--- reclist/__init__.py
+++ reclist/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """RecList"""
 __email__ = ''
-__version__ = '0.2.4'
+__version__ = '0.2.5'
--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jacopotagliabue/reclist',
-    version='0.2.4',
+    version='0.2.5',
     zip_safe=False,
 )
