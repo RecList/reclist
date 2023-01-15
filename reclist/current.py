228380e1e535cd1c742b9f9321fee8b41baa1322
unna97 <31486108+unna97@users.noreply.github.com>
2023-01-15 23:44:36 +0530

    Add typehints to reclist/current.py

--- reclist/current.py
+++ reclist/current.py
@@ -1,9 +1,9 @@
 class Current:
-    def __init__(self):
+    def __init__(self)-> None:
         self._report_path = None
 
     @property
-    def report_path(self):
+    def report_path(self)->str:
         return self._report_path
 
 
